14135698caddc3ba87f11fce585e9649129314b7
Emily Castillo <emilycastillox@gmail.com>
2026-02-18 20:33:28 -0500

    two sum two

--- 2026/week3.js
+++ 2026/week3.js
@@ -10,3 +10,20 @@ const isPalindrome = (s) => {
        }
        return true
     }
+
+const twoSumTwo = (numbers, target) => {
+     let l = 0
+     let r = numbers.length - 1
+
+     while (l < r) {
+        let sum = numbers[l] + numbers[r]
+        if (sum === target){
+            return [l + 1, r + 1]
+        } else if (sum < target){
+            l++
+        } else {
+            r--
+        }
+     }
+
+    }
