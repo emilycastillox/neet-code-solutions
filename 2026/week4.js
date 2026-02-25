4bb429f2e09f15b217c424ac037832a1ad123711
emily <emily@emilys-MacBook-Air.local>
2026-02-25 11:50:11 -0500

    max repeating substrings

--- 2026/week4.js
+++ 2026/week4.js
@@ -19,3 +19,40 @@ const maxProfit = (prices) => {
             }
             return maxProfit;
         }
+
+        const lengthOfLongestSubstring = (s) => {
+        let existingCharacters = new Set();
+        let l = 0
+        let maxLength = 0
+
+        for (let r = 0; r < s.length; r++){
+            while (existingCharacters.has(s[r])){
+                existingCharacters.delete(s[l]);
+                l++;
+            }
+            existingCharacters.add(s[r]);
+            maxLength = Math.max(maxLength, r - l + 1);
+        }
+       return maxLength;
+    }
+
+    const characterReplacement = (s, k) => {
+        let maxLength = 0
+        let count = new Map();
+        
+        let l = 0,  
+            maxf = 0;
+        for (let r = 0; r < s.length; r++){
+            count.set(s[r], (count.get(s[r]) || 0) + 1)
+
+            maxf = Math.max(maxf, count.get(s[r]));
+
+            while (r - l + 1 - maxf > k){
+                count.set(s[l], count.get(s[l]) - 1);
+                l++;
+            }
+            maxLength = Math.max(maxLength, r - l + 1);
+    }
+    return maxLength;
+}
+
